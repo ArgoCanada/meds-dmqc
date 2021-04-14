830ebd5470c90998bb576d12be9c37c1a31660a7
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2021-04-14 13:29:00 -0300

    first pass at an old float

--- dmqc.py
+++ dmqc.py
@@ -2,7 +2,6 @@
 
 from pathlib import Path
 from netCDF4 import Dataset
-
 import bgcArgoDMQC as bgc
 
 # This script will be used to DMQC a given float, as of the current date (feb
@@ -12,3 +11,22 @@ import bgcArgoDMQC as bgc
 # so that there is a clear diary of development on the github page. 
 
 # Good luck me
+
+# this is the first float to do BGC DMQC on
+# deployed off NS shelf in March 2004
+wmo_id = 4900497
+
+# load BOTH the synthetic and individual profile files to redundantly check for
+# differences in gain
+syn  = bgc.sprof(wmo_id)
+# prof = bgc.profiles(wmo_id)
+
+# check the traj file - I doubt there is in-air data on a float that old but 
+# who knows
+# 
+# there is a DOXY variable but not PPOX_DOXY - still, in-air gain will work
+traj = Dataset(syn.__BRtraj__)
+print(traj.variables.keys())
+
+# calculate gains
+woa_gains = syn.calc_gains(ref='WOA')
