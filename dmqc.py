1c90f5e243b2791f9935007b1eb5d65c9441913d
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2021-06-21 09:04:11 -0300

    initial float dmqc

--- dmqc.py
+++ dmqc.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 from netCDF4 import Dataset
 
+from scipy.io import loadmat
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -36,16 +37,17 @@ print(traj.variables.keys())
 woa_gains = syn.calc_gains(ref='WOA')
 
 # make some plots
-g_prof = syn.plot('qcprofiles', varlist=['PSAL', 'TEMP', 'DOXY'])
+g_prof = syn.plot('qcprofiles', varlist=['PSAL_ADJUSTED', 'TEMP_ADJUSTED', 'DOXY_ADJUSTED'])
 syn.clean()
-g_prof2 = syn.plot('qcprofiles', varlist=['PSAL', 'TEMP', 'DOXY'])
+g_prof2 = syn.plot('qcprofiles', varlist=['PSAL_ADJUSTED', 'TEMP_ADJUSTED', 'DOXY_ADJUSTED'])
 
-g_prof.fig.savefig(Path('figures/{}/qcprofiles.png'.format(wmo_id)), dpi=250, bbox_inches='tight')
-g_prof2.fig.savefig(Path('figures/{}/qcprofiles_cleaned.png'.format(wmo_id)), dpi=250, bbox_inches='tight')
+figsize = (g_prof.fig.get_figwidth(), g_prof.fig.get_figheight())
+# g_prof.fig.savefig(Path('figures/{}/qcprofiles.png'.format(wmo_id)), dpi=250, bbox_inches='tight')
+# g_prof2.fig.savefig(Path('figures/{}/qcprofiles_cleaned.png'.format(wmo_id)), dpi=250, bbox_inches='tight')
 
 g_gain = syn.plot('gain', ref='WOA')
 
-g_gain.fig.savefig(Path('figures/{}/gainplot.png'.format(wmo_id)), dpi=250, bbox_inches='tight')
+g_gain.fig.savefig(Path('figures/{}/gainplot_20210621.png'.format(wmo_id)), dpi=250, bbox_inches='tight')
 
 DOXY_ADJUSTED = syn.DOXY * np.nanmean(woa_gains)
 # add to float dict
@@ -64,6 +66,93 @@ ax.plot([np.nan], [np.nan], color=plt.cm.GnBu_r(0.6), label='Python Package')
 
 ax.legend(loc=3, fontsize=8)
 
-fig.savefig(Path('figures/{}/gainprofiles.png'.format(wmo_id)), dpi=250, bbox_inches='tight')
+fig.set_size_inches(figsize[0]/3, figsize[1])
+fig.savefig(Path('figures/{}/gainprofiles_20210621.png'.format(wmo_id)), dpi=250, bbox_inches='tight')
+
+# this float has one anomalous point of surface oxygen - investigate that profile
+surf_sat = syn.__WOAfloatref__[:,2]
+ix = surf_sat > 110
+print(sum(ix))
+g = syn.plot('profiles', varlist=['DOXY'], Ncycle=syn.CYCLE[ix][0]+1, Nprof=1)
+g = syn.plot('qcprofiles', varlist=['DOXY'], Ncycle=syn.CYCLE[ix][0]+1, Nprof=1, axes=g.axes[0])
+g.axes[0].set_ylim((250,0))
+
+g.fig.set_size_inches(figsize[0]/3, figsize[1])
+# g.fig.savefig(Path('figures/{}/anom_profile.png'.format(wmo_id)), dpi=250, bbox_inches='tight')
+
+# load in comparison with SAGE output
+sagefile = Path('/Users/gordonc/Documents/projects/external/ARGO_PROCESSING/MFILES/GUIS/SAGE_O2Argo/cgrdn_sprof/4900497_sagedata.mat')
+sagedata = loadmat(sagefile)
+
+sage_float = sagedata['SURF_SAT'][:,1]
+sage_woa = sagedata['WOAsurf'][0,:]
+py_woa = syn.__WOAref__
+py_float = syn.__WOAfloatref__[:,2]
+
+fig, axes = plt.subplots(1,2)
+axes[0].plot(sage_float, py_float, 'ko')
+axes[1].plot(sage_woa, py_woa, 'ko')
+
+xlim1 = axes[0].get_xlim()
+axes[0].plot(xlim1, xlim1, 'k-')
+axes[0].set_xlim(xlim1)
+axes[0].set_ylim(xlim1)
+axes[0].set_xlabel('SAGE Float % Sat')
+axes[0].set_ylabel('bgcArgoDMQC Float % Sat')
+
+xlim2 = axes[1].get_xlim()
+axes[1].plot(xlim2, xlim2, 'k-')
+axes[1].set_xlim(xlim2)
+axes[1].set_ylim(xlim2)
+axes[1].set_xlabel('SAGE WOA % Sat')
+axes[1].set_ylabel('bgcArgoDMQC WOA % Sat')
+
+fig.set_size_inches(figsize[0]*4/3, figsize[1])
+fig.savefig(Path('figures/{}/sage_comparison_20210621.png'.format(wmo_id)), dpi=250, bbox_inches='tight')
+
+# remove the anomalous point of surface oxygen and recalculate
+iy = np.logical_and(syn.__floatdict__['O2Sat'] > 110, syn.__floatdict__['CYCLE_GRID'] == syn.CYCLE[ix][0])
+syn.__floatdict__['O2Sat'][iy] = np.nan
+
+new_gains = syn.calc_gains(ref='WOA')
+new_gainplot = syn.plot('gain', ref='WOA')
+
+new_gainplot.fig.savefig(Path('figures/{}/new_gainplot_20210621.png'.format(wmo_id)), dpi=250, bbox_inches='tight')
+
+sage_factor = py_float / sage_float
+potential_correct_gain = sage_factor * new_gains
+
+syn.__floatdict__['O2Sat'] = 100*syn.__floatdict__['DOXY'] / bgc.unit.oxy_sol(syn.__floatdict__['PSAL'], syn.__floatdict__['TEMP'], a4330=False)
+
+new_new_gains = syn.calc_gains(ref='WOA')
+new_new_gainplot = syn.plot('gain', ref='WOA')
+
+new_new_gainplot.fig.savefig(Path('figures/{}/new_new_gainplot_20210621.png'.format(wmo_id)), dpi=250, bbox_inches='tight')
+
+py_woa = syn.__WOAref__
+py_float = syn.__WOAfloatref__[:,2]
 
 plt.close('all')
+
+fig, axes = plt.subplots(1,2)
+axes[0].plot(sage_float, py_float, 'ko')
+axes[1].plot(sage_woa, py_woa, 'ko')
+
+xlim1 = axes[0].get_xlim()
+axes[0].plot(xlim1, xlim1, 'k-')
+axes[0].set_xlim(xlim1)
+axes[0].set_ylim(xlim1)
+axes[0].set_xlabel('SAGE Float % Sat')
+axes[0].set_ylabel('bgcArgoDMQC Float % Sat')
+
+xlim2 = axes[1].get_xlim()
+axes[1].plot(xlim2, xlim2, 'k-')
+axes[1].set_xlim(xlim2)
+axes[1].set_ylim(xlim2)
+axes[1].set_xlabel('SAGE WOA % Sat')
+axes[1].set_ylabel('bgcArgoDMQC WOA % Sat')
+
+fig.set_size_inches(figsize[0]*4/3, figsize[1])
+fig.savefig(Path('figures/{}/new_sage_comparison_20210621.png'.format(wmo_id)), dpi=250, bbox_inches='tight')
+
+plt.show()
