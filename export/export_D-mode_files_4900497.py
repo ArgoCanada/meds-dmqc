c03c6e80ffaa2830e27b501af24b4f5de0f4b02e
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2021-11-24 09:32:26 -0400

    small adjustments to export script
    changes still need to be copied to template script

--- export/export_D-mode_files_4900497.py
+++ export/export_D-mode_files_4900497.py
@@ -34,18 +34,30 @@ dims = [
     ('N_PROF')
 ]
 
+# float ID
 wmo = 4900497
-gain = 1.033
+
+# get gain from tracking spreadsheet
+tracker = pd.read_csv(Path('../dmqc_tracker.csv'))
+gain = tracker[tracker.wmo == wmo].gain.iloc[0]
+
+# DMQC operator info
 operater = 'Christopher Gordon'
 affiliation = 'Fisheries and Oceans Canada'
 orcid = '0000-0002-1756-422X'
 
-float_path = Path('/Users/gordonc/Documents/data/Argo/meds') / str(wmo) / 'profiles'
+# grab list of RT files
+float_path = Path('/Users/gordonc/Documents/data/Argo/dac/meds') / str(wmo) / 'profiles'
 R_files = list(float_path.glob('BR*.nc'))
 
+# loop through files
 for fn in R_files:
+    # create DM file
     D_file = fn.as_posix().replace('BR', 'BD')
+    # load RT data
     R_nc = Dataset(fn)
+    # copys RT data except for those variables in exclude_vars or with
+    # dimensions in exclude_dims
     D_nc = bgc.io.copy_netcdf_except(
         fn, D_file, exclude_vars=exclude_vars,
         exclude_dims=exclude_dims
@@ -53,9 +65,10 @@ for fn in R_files:
     # create N_CALIB one greater than the previous one
     D_nc.createDimension('N_CALIB', size=R_nc.dimensions['N_CALIB'].size+1)
     # create the variables that you removed, but with the new N_CALIB
-    for v,d in zip(exclude_vars, dims):
+    for v, d in zip(exclude_vars, dims):
         D_nc.createVariable(
-            v, R_nc[v].datatype, fill_value=R_nc[v]._FillValue,
+            v, R_nc[v].datatype, 
+            fill_value=R_nc[v]._FillValue,
             dimensions=d
         )
         D_nc[v].setncatts(R_nc[v].__dict__)
@@ -67,6 +80,7 @@ for fn in R_files:
         dtype=D_nc['SCIENTIFIC_CALIB_COMMENT'].datatype
     )
 
+    # add scientific calibration comment
     comment = 'Oxygen gain calculated following Johnson et al. 2015, doi:10.1175/JTECH-D-15-0101.1, using comparison between float and WOA data. Adjustment applied by {} ({}, orcid: {})'.format(operater, affiliation, orcid)
     N = len(comment)
     M = 256 - N
@@ -75,12 +89,12 @@ for fn in R_files:
     scientific_calib_comment[0, 0, -1, :] = comment
     D_nc['SCIENTIFIC_CALIB_COMMENT'][:] = scientific_calib_comment
     
+    # add scientific calibration equation
     scientific_calib_equation = np.full(
         D_nc['SCIENTIFIC_CALIB_EQUATION'].shape,
         D_nc['SCIENTIFIC_CALIB_EQUATION']._FillValue,    
         dtype=D_nc['SCIENTIFIC_CALIB_EQUATION'].datatype
     )
-
     equation = 'DOXY_ADJUSTED = G*DOXY'
     N = len(equation)
     M = 256 - N
@@ -95,6 +109,7 @@ for fn in R_files:
         dtype=D_nc['SCIENTIFIC_CALIB_COEFFICIENT'].datatype
     )
 
+    # add gain coefficient
     coeff = 'G = {}'.format(gain)
     N = len(coeff)
     M = 256 - N
@@ -104,6 +119,7 @@ for fn in R_files:
     scientific_calib_coefficient[0, 1, 2, :] = coeff
     D_nc['SCIENTIFIC_CALIB_COEFFICIENT'][:] = scientific_calib_coefficient
 
+    # populate DOXY_ADJUSTED
     doxy_adjusted = gain*D_nc['DOXY'][:].data
     D_nc['DOXY_ADJUSTED'][:] = doxy_adjusted
     doxy_adjusted_qc = D_nc['DOXY_QC'][:].data
@@ -114,13 +130,15 @@ for fn in R_files:
     D_nc['DOXY_ADJUSTED_QC'][:] = doxy_adjusted_qc
     D_nc['DOXY_ADJUSTED'][:][doxy_adjusted_qc == b'4'] = D_nc['DOXY_ADJUSTED']._FillValue
 
-    molar_doxy_adjusted = gain*D_nc['MOLAR_DOXY'][:].data
-    D_nc['MOLAR_DOXY_ADJUSTED'][:] = molar_doxy_adjusted
-    molar_doxy_adjusted_qc = D_nc['MOLAR_DOXY_QC'][:].data
-    molar_doxy_adjusted_qc[molar_doxy_adjusted_qc == b'3'] = b'2'
-    D_nc['MOLAR_DOXY_ADJUSTED_QC'][:] = molar_doxy_adjusted_qc
+    # don't worry about MOLAR_DOXY anymore
+    # molar_doxy_adjusted = gain*D_nc['MOLAR_DOXY'][:].data
+    # D_nc['MOLAR_DOXY_ADJUSTED'][:] = molar_doxy_adjusted
+    # molar_doxy_adjusted_qc = D_nc['MOLAR_DOXY_QC'][:].data
+    # molar_doxy_adjusted_qc[molar_doxy_adjusted_qc == b'3'] = b'2'
+    # D_nc['MOLAR_DOXY_ADJUSTED_QC'][:] = molar_doxy_adjusted_qc
 
     # D_nc['PARAMETER'][:] = R_nc['PARAMETER'][:]
+    # NOTE: this is manual right now, long term should not be
     D_nc['DATA_MODE'][:] = np.array([b'D'])
     D_nc['PROFILE_DOXY_QC'][:] = np.array([b'D'])
     D_nc['PARAMETER_DATA_MODE'][:] = np.array([b'R', b'D', b'R', b'D'])
@@ -134,8 +152,9 @@ for fn in R_files:
     T = sprof_nc['TEMP'][:]
     P = D_nc['PRES'][:]
 
+    # populate adjusted error
     D_nc['DOXY_ADJUSTED_ERROR'][:] = bgc.unit.pO2_to_doxy(4, S, T, P)
-    D_nc['MOLAR_DOXY_ADJUSTED_ERROR'][:] = bgc.unit.umol_per_sw_to_mmol_per_L(D_nc['DOXY_ADJUSTED_ERROR'][:].data, S, T, P)
+    # D_nc['MOLAR_DOXY_ADJUSTED_ERROR'][:] = bgc.unit.umol_per_sw_to_mmol_per_L(D_nc['DOXY_ADJUSTED_ERROR'][:].data, S, T, P)
 
     R_nc.close()
     D_nc.close()
