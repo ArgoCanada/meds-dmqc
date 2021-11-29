624ac9930eaee5dc8b2cf3ebc22bc2d151a17338
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2021-11-29 09:32:07 -0400

    run checks on 4900524 and 4900627

---
+++ check/check_4900627.sh
@@ -0,0 +1,10 @@
+wmo=4900627
+path=c:/Users/GordonC/Documents/data/Argo/dac/meds/D/$wmo/profiles
+logfile=../log/log_$wmo.xml
+echo "***FileChecker log for float $wmo***" > $logfile
+echo >> $logfile
+for f in $path/BD*.nc; do
+    echo $f >> $logfile
+    python argo-checker.py check $f >> $logfile
+    echo >> $logfile
+done
