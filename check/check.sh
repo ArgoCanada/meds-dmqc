5b87219c81dc55a2284e183170b58d0b7e7b43a8
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2025-09-25 12:52:17 -0300

    fix errors from updated filechecker version

--- check/check.sh
+++ check/check.sh
@@ -3,5 +3,5 @@ path=c:/Users/GordonC/Documents/data/Argo/dac/meds/D/$wmo/profiles
 mkdir ./out/$wmo
 files=`ls $path/BD*.nc > tmp.txt`
 echo $files
-java -jar ValidateSubmit.jar meds ./spec/ ./out/$wmo/ $path $files
+java -jar file_checker_exec-2.9.2.jar meds ./file_checker_spec/ ./out/$wmo/ $path $files
 python summarize_filecheck.py $wmo
